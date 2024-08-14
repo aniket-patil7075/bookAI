74e8501e7d82eab196316f09519b8f57eed5ec41
aniket-patil7075 <akp7075@gmail.com>
2024-08-14 12:34:08 +0530

    Navbar done

---
+++ src/component/NavigBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+function NavigBar() {
+  return (
+    <div className='navigbar'>
+        <Navbar className='navbar' > 
+        <Container>
+          <Nav className="me-auto navbar">
+            <Nav.Link className='abc' href="#overview">OVERVIEW</Nav.Link>
+            <Nav.Link className='abc' href="#authentication">AUTHENTICATION</Nav.Link>
+            <Nav.Link className='abc' href="#endpoints">ENDPOINTS</Nav.Link>
+            <Nav.Link className='abc' href="#tutorial">TUTORIAL</Nav.Link>
+            <Nav.Link className='abc' href="#code">CODE EXAMPLES</Nav.Link>
+            <Nav.Link className='abc' href="#pricing">PRICING</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  )
+}
+
+export default NavigBar
---
+++ src/component/Overview.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Modal from 'react-bootstrap/Modal';
+
+function Overview() {
+  return (
+    <div>
+        {/* <div>
+        <h3>Overview</h3>
+        <p>The Book Generator API allows you to generate books on various topics using different language models. This documentation provides details on how to use the API, including authentication, available endpoints, and code examples.</p>
+        </div> */}
+    </div>
+  )
+}
+
+export default Overview
